082ea7aa79790a707d8ac03c04e47825e37b0614
Piyush_Git <pk29200405@gmail.com>
2025-07-03 11:23:19 +0530

    this are codes

--- day2/src/tnsif/day2/identifierdemo.java
+++ day2/src/tnsif/day2/identifierdemo.java
@@ -4,6 +4,25 @@ public class identifierdemo {
 
 	public identifierdemo() {
 		// TODO Auto-generated constructor stub
+		//variable declaration=invalid identifier examples
+		
+				//int for=13; error bcz keyword cannot be declare as variable
+				
+				//int num l= 13;erroe bcz of spaces
+				
+				//int 0num = 13;bcz special characters are not allowed
+				
+				//valid identification
+				
+				int $num1= 20;
+				
+				String StudentName="Heli";
+				
+				int FOR =12;
+				
+				System.out.println("Values of the number variables is: "+ StudentName);
+				
+				System.out.println("Student Name is : "+ StudentName);
 	}
 
 }
