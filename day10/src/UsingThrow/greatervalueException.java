747df19b26637de095f8d03b764a4eaf88591efd
Piyush_Git <pk29200405@gmail.com>
2025-07-10 19:11:43 +0530

    this are codes

---
+++ day10/src/UsingThrow/greatervalueException.java
@@ -0,0 +1,15 @@
+package UsingThrow;
+
+public class greatervalueException extends Exception {
+
+	
+	public greatervalueException(String string)
+	{
+		super(string);
+	}
+	
+	public greatervalueException()
+	{
+		super("Percentage should not be more then 100");
+	}
+}
