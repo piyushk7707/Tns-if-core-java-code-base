ee6b36f182516769d9bfb01981c8bb583ab90fec
Piyush_Git <pk29200405@gmail.com>
2025-07-14 09:16:17 +0530

    this are day 13 codes

---
+++ day13/src/tnsif/day13syncronisation/InsufficientBalanceException.java
@@ -0,0 +1,14 @@
+package tnsif.day13syncronisation;
+
+public class InsufficientBalanceException extends Exception {
+
+	public InsufficientBalanceException()
+	{
+		super("Insufficient balance in your account");
+	}
+	
+	public InsufficientBalanceException(String message)
+	{
+		super(message);
+	}
+}
