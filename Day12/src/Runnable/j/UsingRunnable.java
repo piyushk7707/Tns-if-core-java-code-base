3c640e3bcf24e9725e082b112aaeb636c9c1b5d4
Piyush_Git <pk29200405@gmail.com>
2025-07-14 09:15:13 +0530

    this are day 12 codes

---
+++ Day12/src/Runnable/j/UsingRunnable.java
@@ -0,0 +1,34 @@
+package Runnable.j;
+
+public class UsingRunnable implements Runnable{
+
+	Thread t;
+	int end, start;
+	String msg;
+
+	public UsingRunnable(int end, int start, String msg) {
+		super();
+		this.end = end;
+		this.start = start;
+		this.msg = msg;
+	}
+
+	@Override
+	public void run() {
+		
+		for(int i=end; i>start; i--)
+		{
+			try {
+				Thread.sleep(500);
+				
+			}
+			catch(InterruptedException e)
+			{
+				System.err.println(e.getMessage());
+			}
+			System.out.println(i + msg);
+		}
+		
+	}
+
+}
